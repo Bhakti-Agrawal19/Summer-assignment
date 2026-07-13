2ede455847375bc56cca18e5518a8b0179205367
Bhakti-Agrawal19 <bhakti.agrawal2025@glbajajgroup.org>
2026-07-13 13:29:59 +0530

    Add matrix operations including multiplication and sums

---
+++ dat20.c
@@ -0,0 +1,198 @@
+
+// Q77	Write a program to Multiply matrices.	TCS, Infosys, Wipro
+//   Q78	Write a program to Check symmetric matrix.	TCS, Infosys, Wipro
+//   Q79	Write a program to Find row-wise sum.	TCS, Infosys, Wipro
+//   Q80	Write a program to Find column-wise sum.	TCS, Infosys, Wipro
+
+
+
+
+// ans1
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], b[10][10], c[10][10];
+    int r1, c1, r2, c2;
+    int i, j, k;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1 != r2)
+    {
+        printf("Matrix multiplication is not possible.");
+        return 0;
+    }
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c1; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r2; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Matrix Multiplication
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            c[i][j] = 0;
+            for (k = 0; k < c1; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("\nResultant Matrix:\n");
+    for (i = 0; i < r1; i++)
+    {
+        for (j = 0; j < c2; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// ans2
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], transpose[10][10];
+    int i, j, row, col, symmetric = 1;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    if (row != col)
+    {
+        printf("Matrix is not symmetric.");
+        return 0;
+    }
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Find transpose
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    // Compare matrix with its transpose
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (a[i][j] != transpose[i][j])
+            {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("Matrix is symmetric.");
+    else
+        printf("Matrix is not symmetric.");
+
+    return 0;
+}
+
+// ans3
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int i, j, row, col, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nRow-wise sum:\n");
+    for (i = 0; i < row; i++)
+    {
+        sum = 0;
+        for (j = 0; j < col; j++)
+        {
+            sum = sum + a[i][j];
+        }
+        printf("Sum of Row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
+
+// ans4
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int i, j, row, col, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nColumn-wise sum:\n");
+    for (j = 0; j < col; j++)
+    {
+        sum = 0;
+        for (i = 0; i < row; i++)
+        {
+            sum = sum + a[i][j];
+        }
+        printf("Sum of Column %d = %d\n", j + 1, sum);
+    }
+
+    return 0;
+}
